081f13cc604d8dadd7def418f83065d765070307
bhavana1308 <b.bhavanareddy17@gmail.com>
2023-07-15 14:18:02 -0500

    necessary code changes

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styling.css";
+
+export default function Banner() {
+    return (
+        <div>
+            <h1 id="banner">Orbit Report</h1>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
